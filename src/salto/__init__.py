7921bdd849844a351df4f8a10b321053929e227b
Mc-Zen <alphabetagamma11@hotmail.com>
2024-07-22 14:10:16 +0200

    add single-shot estimators

--- src/salto/__init__.py
+++ src/salto/__init__.py
@@ -1,2 +1,3 @@
 
 from .matrices import M, M1, M2, T1, iT1, T2, iT2, T3, iT3
+from .single_shot import single_shot_estimators
---
+++ src/salto/single_shot.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+import numpy as np
+from . import matrices
+
+
+def single_shot_estimators(n: int) -> Tuple[np.ndarray]:
+    """Generate $n$-qubit single-shot estimators
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+
+    Returns
+    -------
+    Tuple[np.ndarray]
+        Returns a tuple of 6 matrices containing single-shot estimators for
+        the quantum weight distributions a, b, a', b', a'', and b'', respectively. 
+        Each matrix consists of $n+1$ estimators as columns where the column index
+        corresponds to the number of measured singlets. 
+    """
+    M2 = matrices.M2(n)
+    iT2 = matrices.iT2(n)
+    iT3 = matrices.iT3(n)
+    return iT2, iT2 @ M2, iT3, np.flipud(iT3), np.eye(n + 1), M2
